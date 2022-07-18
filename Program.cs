50f63919d494300df7c8cd3d1a8de41a6c97d174
Luis-Antonimo <luisantonimo@gmail.com>
2022-07-19 00:36:57 +0200

    ok

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace T09_Ejercicio9
+{
+    class Electrodomestico
+    {
+        //No sé a qué se refiere con "Indica que se podrán heredar"
+        private double precioBase;
+        private string color;
+        private string consumoEnerg;
+        private double peso;
+
+        //No se a qué se refiere con declara constantes en el constructor
+        //por defecto
+        public Electrodomestico()
+        {
+            precioBase = 100;
+            color = "blanco";
+            consumoEnerg = "F";
+            peso = 5;
+        }
+
+        public Electrodomestico(double precioNew, double pesoNew)
+        {
+            precioBase = precioNew;
+            peso = pesoNew;
+            consumoEnerg = "F";
+            peso = 5;
+        }
+
+        public Electrodomestico(double precioNew, double pesoNew, string colorNew, string consumoEnergNew)
+        {
+            precioBase = precioNew;
+            peso = pesoNew;
+            color = colorNew;
+            consumoEnerg = consumoEnergNew;
+        }
+    }
+}
